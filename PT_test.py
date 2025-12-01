e8439259db705b26bbc4a171f97d9a79ce0ccf92
Arturmn9 <artur.mnatsakanyan.17@gmail.com>
2025-12-01 17:53:27 -0600

    PT_test.py

--- PT_test.py
+++ PT_test.py
@@ -1,23 +1,25 @@
 # import re
 import tokenize
 import io
-# KEY_MAP = {
-#     "grir": "print",
-#     "qani": "while",
-#     "ete": "if",
-#     "urish": "else",
-#     "veradarcru": "return",
-#     "sahmanel": "def"
-# }
-# KEY_MAP = {
-#     "imprimir": "print",
-#     "mientras": "while",
-#     "si": "if",
-#     "demas": "else",
-#     "devolver": "return",
-#     "definir": "def"
-# }
-KEY_MAP = {
+# Spanish Dictionary 
+KEY_MAP_SPA = {
+    "imprimir": "print",
+
+    "mientras": "while",
+
+    "si": "if",
+
+    "demás": "else",
+    "demÃ¡s": "else",
+
+    "devolver": "return",
+
+    "definir": "def"
+
+}
+
+# Armenian Dictionary 
+KEY_MAP_ARM = {
     "տպել": "print",
     "Õ¿ÕºÕ¥Õ¬": "print",
 
@@ -40,23 +42,28 @@ KEY_MAP = {
     "Õ½Õ¡Õ°Õ´Õ¡Õ¶Õ¥Õ¬": "def"
 }
 
-# def preprocessor_test(source_code, keyword_map):
-#     f_p = open(source_code, "r")
-#     user_code = f_p.read()
-#     f_p.close()
-    
-#     for keyWord in keyword_map:
-#         user_code = re.sub(r'\b' + re.escape(keyWord) + r'\b', re.escape(keyword_map[keyWord]), user_code)
-#     return user_code
-
 def transform_line(line, start, end, new_word):
+    # Purpose - Replaces single word in a given line with a given word
 
+    # Parameters -
+    #     line: (STRING) a string where the replacement should happen
+    #     start: (INTEGER) a beginning index of the word
+    #     end: (INTEGER) an end index of the word
+    #     new_word: (STRING) the new line that has to be placed in the space
+
+    # Return - The new line where the word is replaced
     list_look = line.split(line[start : end],1)
     
     return f"{new_word}".join(list_look)
 
 
 def preprocessor_test(source_code, keyword_map):
+    # Purpose - It takes a source code and it translates the keywords written in different language into an executable code.
+    
+    # Parameters -
+    #    source_code: (STRING) Name of the file that contains the initial raw code. Either TXT or PY
+    #    keyword_map: (DICTIONARY) Name of the language dictionary that the user wants the code to be translated from
+    # Return - The translated code as a string. 
     f_p = open(source_code, "r")
     output_string = ''
     for line in f_p:
@@ -70,7 +77,8 @@ def preprocessor_test(source_code, keyword_map):
     return output_string
 
 
-exec(preprocessor_test("test_p.py", KEY_MAP))
+exec(preprocessor_test("test_arm_python.py", KEY_MAP_ARM))
+exec(preprocessor_test("test_spa_python.py", KEY_MAP_SPA))
 
 
 
